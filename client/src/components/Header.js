6172f96b12850cc8f0c12ce0aeb8c3ad33a4dc21
Kevin Locke <kevinbreid@yahoo.com>
2023-05-26 11:56:41 -0700

    styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,20 @@
 import SearchResult from "./SearchResult"
+import avatar from "../images/avatar.png"
 
 const Header = () => {
 
     return(
 
         <div className="header-container">
-            <div className="header-title">
-                <h1>CRYPTOCURRENCY</h1>
-                <h7>A Daily Crypto Organizer</h7>
+            <div className="header-title-container">
+                <div className="space-div-left"></div>
+                <div className="header-title">
+                    <h1>CRYPTOCURRENCY</h1>
+                    <h7>A Daily Crypto Organizer</h7>
+                </div>
+                <div className="space-div-right">
+                    <img className="avatar" src={avatar}/>
+                </div>
             </div>
            
                 <input className="search-input" placeholder="Search Currency"/>
